42943fe80d61b019dd12ba4d6b097a9ffcd9794b
Brandonmorsework <brandonmorsework@gmail.com>
2025-04-10 09:27:49 -0400

    initial commit

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("oranges");
+        System.out.println("banana");
+        System.out.println("bread");
+        System.out.println("sugar");
+        System.out.println("coffee beans");
+        System.out.println("eggs");
+        System.out.println("butter");
+        System.out.println("mushrooms");
+        System.out.println("waffles");
+        System.out.println("ham"); }
+
+    }
