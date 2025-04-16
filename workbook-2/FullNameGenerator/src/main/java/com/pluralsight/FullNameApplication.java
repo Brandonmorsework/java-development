aa3516524911e2378b6c9468f447e6ab5d3ed982
Brandonmorsework <brandonmorsework@gmail.com>
2025-04-16 15:02:07 -0400

    Updated FullNameGenerator, including fixes using .trim

--- workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -9,15 +9,38 @@ public class FullNameApplication {
         System.out.println("First Name: ");
         String firstName = namescanner.nextLine();
 
+        if (firstName.trim().isEmpty()) {
+            System.out.println("First Name is Required");
+            return;
+        }
         System.out.println("Middle Name: ");
         String middleName = namescanner.nextLine();
+
+        if (middleName.trim().isEmpty()) {
+            middleName = ("");
+        } else  {
+            middleName = (" " + middleName.trim());
+
+        }
 
         System.out.println("Last Name: ");
         String lastName = namescanner.nextLine();
 
-        System.out.println("Last Name: ");
+        if (lastName.trim().isEmpty()) {
+            System.out.println("Last Name is Required");
+            return;
+        }
+
+        System.out.println("Enter Suffix (If you have one): ");
         String suffixEntry = namescanner.nextLine();
 
-        System.out.println(firstName + " " + middleName + " " + lastName + suffixEntry);
+        if (suffixEntry.trim().isEmpty()) {
+            suffixEntry = ("");
+        } else  {
+            suffixEntry = (", " + suffixEntry);
+
+        }
+
+        System.out.println(firstName.trim() + middleName + " " + lastName.trim() + suffixEntry.trim());
     }
 }
