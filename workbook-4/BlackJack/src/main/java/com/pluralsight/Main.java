010db2701efaac8e870568534560e33a8ccb84f4
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-08 16:08:58 -0400

    added deck.deal to player 1 and 2's hand twice

--- workbook-4/BlackJack/src/main/java/com/pluralsight/Main.java
+++ workbook-4/BlackJack/src/main/java/com/pluralsight/Main.java
@@ -1,13 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String playerA;
-        String playerB;
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter Player 1's name: ");
+        String playerA = scanner.nextLine();
+        System.out.println("Enter Player 2's name: ");
+        String playerB = scanner.nextLine();
 
+        Deck deck1 = new Deck();
+        deck1.shuffle();
+        Hand player1Hand = new Hand();
+        Hand player2Hand = new Hand();
+
+        player1Hand.deal(deck1.deal());
+        player1Hand.deal(deck1.deal());
+
+        player2Hand.deal(deck1.deal());
+        player2Hand.deal(deck1.deal());
     }
 
 }
