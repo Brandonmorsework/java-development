aae3579a60c98548745214dacf35b5aa391ababe
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-08 10:47:18 -0400

    first step of unit testing (will return)

---
+++ workbook-4/HotelOperations2/src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RoomTest {
+    @Test
+    public void checkIn_should_isDirtyandisOccupied() {
+
+    //arrange
+
+
+    //act
+
+
+    //assert
+
+
+    }
+
+    @Test
+    public void checkIn_should_isOccupied() {
+
+        //arrange
+
+
+        //act
+
+
+        //assert
+
+
+    }
+
+
+}
