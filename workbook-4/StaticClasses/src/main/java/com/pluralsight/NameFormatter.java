a630992e18cad7c8af667535ee2c06bc662f7600
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-08 10:51:45 -0400

    Started Static Classes

---
+++ workbook-4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class NameFormatter {
+
+    private NameFormatter() {
+    }
+        public static String format(String firstName, String lastName) {
+
+    }
+
+
+
+
+}
