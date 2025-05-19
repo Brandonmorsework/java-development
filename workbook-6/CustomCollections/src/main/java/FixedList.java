d556e4432cd7bd08a921e65f3c3548c61be0d8f0
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-19 14:18:22 -0400

    cleaned up Custom collection declarations

--- workbook-6/CustomCollections/src/main/java/FixedList.java
+++ workbook-6/CustomCollections/src/main/java/FixedList.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class FixedList<T> {
 
-    private T List<T>
-    private T maxSize;
+    private  List<T> items;
+    private int maxSize;
+
+    public FixedList(List<T> items, int maxSize) {
+        this.items = new ArrayList<>();
+        this.maxSize = maxSize;
+    }
+
 
 
 
