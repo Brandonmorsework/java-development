6c0b0b4c40f80efcd60587191ccc303f56d043d9
Brandonmorsework <brandonmorsework@gmail.com>
2025-06-11 08:47:24 -0400

    updated northwindtraders2

--- workbook-8/NorthwindTraders2/src/main/java/com/pluralsight/NorthwindTraders2.java
+++ workbook-8/NorthwindTraders2/src/main/java/com/pluralsight/NorthwindTraders2.java
@@ -1,9 +1,15 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class NorthwindTraders2 {
     public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet results = null;
+
+
         try {
             if (args.length != 2) {
                 System.out.println(
@@ -16,45 +22,68 @@ public class NorthwindTraders2 {
             String username = args[0];
             String password = args[1];
 
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please Enter the last name of the customers you want to see: ");
+            String lastNameToSee = scanner.nextLine();
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
 
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/northwind",
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/sakila",
                     username,
                     password);
 
 
-            String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
+            String query = """
+            SELECT first_name, last_name
+            FROM customer
+            WHERE last_name LIKE ?
+            ORDER BY first_name
+            """;
 
 
             PreparedStatement statement = connection.prepareStatement(query);
 
 
-            ResultSet results = statement.executeQuery();
+            results = statement.executeQuery();
 
 
             while (results.next()) {
-                int productId = results.getInt("ProductID");
-                String productName = results.getString("ProductName");
-                double unitPrice = results.getDouble("UnitPrice");
-                int unitsInStock = results.getInt("UnitsInStock");
+                String firstName = results.getString("first_name");
+                String lastName = results.getString("last_name");
 
-                System.out.println("Product ID: " + productId);
-                System.out.println("Product Name: " + productName);
-                System.out.println("Unit Price: " + unitPrice);
-                System.out.println("Units In Stock: " + unitsInStock);
+                System.out.println("FirstName: " + firstName);
+                System.out.println("LastName: " + lastName);
                 System.out.println("-----------------------------------------");
             }
 
-
-            results.close();
-            statement.close();
-            connection.close();
-
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+
+        } finally {
+            if (results != null) {
+                try {
+                    results.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
